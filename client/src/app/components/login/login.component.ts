120c84cc47107c195d058f8479e05e9fc2e82436
yesusbc <yesus.becerril@gmail.com>
2020-08-16 17:57:35 -0500

    users identifying and login form

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,16 +1,80 @@
 import { Component, OnInit} from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
 	selector: 'login',
-	templateUrl: './login.component.html'
+	templateUrl: './login.component.html',
+	providers: [UserService]
 })
 
 export class LoginComponent implements OnInit{
 	public title:string;
-	constructor(){
-		this.title = 'Identificate';
+	public user:User;
+	public status:string;
+	public identity;
+	public token;
+
+	constructor(
+		private _route: ActivatedRoute,
+		private _router: Router,
+		private _userService: UserService
+	){
+		this.title = 'Login';
+		this.user = new User( "","","","","",0,0,"","","","","");
 	}
 	ngOnInit(){
 		console.log('Componente de login cargado...');
 	}
+
+	onSubmit(){
+		// log user and get its data
+		this._userService.signup(this.user).subscribe(
+			response => {
+				this.identity = response.user;
+				console.log(this.identity);
+				if(!this.identity || !this.identity._id){
+					this.status = 'error';
+				}else{
+					this.status = 'success';	
+					// Pending to save on storage, persisting data
+
+					// get token
+					this.getToken();
+				}
+			},
+			error => {
+				var errorMessage = <any>error;
+				console.log(errorMessage);
+				if(errorMessage != null){
+					this.status = 'error';
+				}
+			}
+		);
+	}
+
+	getToken(){
+		this._userService.signup(this.user, 'true').subscribe(
+			response => {
+				this.token = response.token;
+				if(this.token.length <= 0){
+					this.status = 'error';
+				}else{
+					this.status = 'success';	
+					// Pending to save on storage, persisting token
+
+					// get counters and statistics of user
+				}
+			},
+			error => {
+				var errorMessage = <any>error;
+				console.log(errorMessage);
+				if(errorMessage != null){
+					this.status = 'error';
+				}
+			}
+		);
+	}
+
 }
--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -19,4 +19,15 @@ export class UserService{
 		return this._http.post(this.url+'register', params, {headers: headers});
 
 	}
+
+	signup(user, gettoken = null): Observable<any>{
+		if(gettoken != null){
+			user.gettoken = gettoken;
+		}
+
+		let params = JSON.stringify(user);
+		let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+		return this._http.post(this.url+'login', params, {headers: headers});
+	}
 }
